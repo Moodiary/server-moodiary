0bcdb7899eb5cce1cace76c857dde72f84d17a46
soohyun <soohyun6879@naver.com>
2021-05-27 16:44:42 +0900

    add: connect aws

--- config/db_config.js
+++ config/db_config.js
@@ -8,22 +8,22 @@ const db_info = {
 	dev:{
 		host: 'localhost',
 		user: 'test',
-		password: process.env.DB_SECRET,
+		password: process.env.DB_SECRET, 
 		database: 'moodiary'
     },
     // 실제 운영 서버 환경
-	real:{
-		host: '',
-		port: '',
-		user: '',
-		password : '',
-		database : ''
+	real: {
+		host: 'moodiary-db.ct31r010pyis.us-east-2.rds.amazonaws.com',
+        port: 3306,
+        user: 'moodiary',
+        password : process.env.DB_SECRET,
+        database : 'moodiary'
 	}	
 };
 
 const db_connection = {
 	init : function(){
-        return mysql.createConnection(db_info.dev);
+        return mysql.createConnection(db_info.real);
 	}
 };
 
